01779003037b51b6744f6541e76192819cefde36
AERO <aeroassault6@gmail.com>
2022-08-05 17:02:30 -0400

    Update README, ModPlugin updated to allow for adding to active saves

--- src/data/scripts/stg_ModPlugin.java
+++ src/data/scripts/stg_ModPlugin.java
@@ -2,6 +2,7 @@ package data.scripts;
 
 import com.fs.starfarer.api.BaseModPlugin;
 import com.fs.starfarer.api.Global;
+import com.fs.starfarer.api.campaign.CharacterDataAPI;
 import com.fs.starfarer.api.campaign.rules.MemoryAPI;
 import data.campaign.econ.abilities.CryosleeperScanner;
 import data.campaign.econ.abilities.GateScanner;
@@ -13,10 +14,24 @@ public class stg_ModPlugin extends BaseModPlugin {
     @Override
     public void onGameLoad(boolean newGame){
         MemoryAPI sectorMemory = Global.getSector().getMemoryWithoutUpdate();
-        if (newGame){
+        CharacterDataAPI characterData = Global.getSector().getCharacterData();
+        if (!sectorMemory.contains(GateScanner.UNSCANNED_GATES)) {
             sectorMemory.set(GateScanner.UNSCANNED_GATES, true);
+        }
+        if (!sectorMemory.contains(HypershuntScanner.CAN_SCAN_HYPERSHUNTS)) {
             sectorMemory.set(HypershuntScanner.CAN_SCAN_HYPERSHUNTS, true);
+        }
+        if (!sectorMemory.contains(CryosleeperScanner.CAN_SCAN_CRYOSLEEPERS)) {
             sectorMemory.set(CryosleeperScanner.CAN_SCAN_CRYOSLEEPERS, true);
+        }
+        if (!characterData.getAbilities().contains("stg_GateScanner")) {
+            characterData.addAbility("stg_GateScanner");
+        }
+        if (!characterData.getAbilities().contains("stg_HypershuntScanner")) {
+            characterData.addAbility("stg_HypershuntScanner");
+        }
+        if (!characterData.getAbilities().contains("stg_CryosleeperScanner")) {
+            characterData.addAbility("stg_CryosleeperScanner");
         }
     }
 }
